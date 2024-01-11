a884f8c956d7f2a4273bef8035fd7428b3784176
DavidTello1 <davidtellop@gmail.com>
2024-01-11 16:35:28 +0100

    update pages

--- js/agency.js
+++ js/agency.js
@@ -6,16 +6,6 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-$(function() {
     function changeSection(sectionId) {
         $('#projects, #about').hide();
         $('#' + sectionId).show();
